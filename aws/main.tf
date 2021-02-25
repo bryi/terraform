b2ec530b10a17d9cfca393ddcc71591a5ff36cd4
root <root@bryivm.moscow.ctm.ru>
2021-02-25 17:51:23 +0300

    delete cred

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-    access_key = "AKIATUJLS22QWWJAFGUB"
-    secret_key = "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"
     region = "us-east-1"
 }
 
 resource "aws_instance" "my-test-1" {
-  count = 3
+  count = 1
   ami           = "ami-0915bcb5fa77e4892"
   instance_type = "t2.micro"
 
