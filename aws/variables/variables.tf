68b743800733cc99c0684cf569b20c48f6993d3f
root <root@bryivm.moscow.ctm.ru>
2021-03-03 15:54:07 +0300

    add variables

---
+++ aws/variables/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "Please Enter AWS Region to deploy Server"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Enter Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+
+variable "allow_ports" {
+  description = "List of Ports to open for server"
+  type        = list
+  default     = ["80", "443", "22"]
+}
+
+variable "enable_detailed_monitoring" {
+  type    = bool
+  default = false
+}
+
+
+variable "common_tags" {
+  description = "Common Tags to apply to all resources"
+  type        = map
+  default = {
+    Owner       = "Alexey Bryi"
+    Project     = "Phoenix"
+    CostCenter  = "12345"
+    Environment = "development"
+  }
+}
